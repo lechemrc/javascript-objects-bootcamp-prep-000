d77a3b5899f7abeacd25e6cf6569ee22910b9ef3
Robert LeCheminant <lecheminant@gmail.com>
2018-03-28 19:54:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({ Radiohead: 'There, There', SufjanStevens: 'Impossible Soul', StVincent: 'Marrow'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  
+  playlist.
 }
