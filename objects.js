16d7b9a9c3c3335214cf1001c13e8f5520eb2a92
Robert LeCheminant <lecheminant@gmail.com>
2018-03-28 19:51:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,1 @@
-var playlist = new Object({ Radiohead: 'There, There', SufjanStevens: 'Impossible Soul'});
+var playlist = new Object({ Radiohead: 'There, There', SufjanStevens: 'Impossible Soul', StVincent: 'Marrow'});
